5d211d3218a6c6b312d69a7bbfe5e9e21fbd3773
Pawel <zacdbg@gmail.com>
2017-01-02 17:55:42 +0100

    v7.1.0 - Initial legion build

--- main.lua
+++ main.lua
@@ -1,28 +1,29 @@
-﻿-- Author      : Kaminari
--- Create Date : 13:03 2015-04-20
-
--- Spells
-local _ExecutionSentence = 114157;
-local _FinalVerdict = 157048;
-local _DivineStorm = 53385;
-local _HammerofWrath = 24275;
-local _CrusaderStrike = 35395;
+﻿-- Spells
+local _TemplarsVerdict = 85256;
 local _Judgment = 20271;
-local _Exorcism = 879;
+local _CrusaderStrike = 35395;
+local _BladeofJustice = 184575;
+local _AshestoAshes = 179546;
+local _WakeofAshes = 205273;
+local _Crusade = 224668;
 local _AvengingWrath = 31884;
-local _Seraphim = 152262;
-local _TemplarVerdict = 85256;
-local _HolyWrath = 119072;
-local _AvengersShield = 31935;
-local _LightsHammer = 114158;
-local _HolyPrism = 114165;
+local _DivineStorm = 53385;
+local _Heroism = 32182;
+local _Bloodlust = 2825;
+local _TimeWarp = 80353;
+local _ShieldofVengeance = 184662;
+local _MasteryDivineJudgment = 76672;
+local _JusticarsVengeance = 215661;
+local _DivinePurpose = 223817;
+local _GreaterBlessingofMight = 203528;
+local _HolyWrath = 210220;
+local _Zeal = 217020;
+local _BlindingLight = 115750;
+local _FinalVerdict = 198038;
+local _ExecutionSentence = 213757;
+local _EyeforanEye = 205191;
 local _Consecration = 26573;
-local _ShieldoftheRighteous = 53600;
-local _SacredShield = 20925;
-
--- Auras
-local _EmpoweredDivineStorm = 174718;
-local _DivineCrusader = 144595;
+local _DivineHammer = 198034;
 
 -- Talents
 local _isFinalVerdict = false;
@@ -31,218 +32,124 @@ local _isSanctifiedWrath = false;
 local _isExecutionSentence = false;
 local _isLightsHammer = false;
 local _isHolyPrism = false;
+local _isZeal = false;
 local _isSacredShield = false;
+local _isBlindingLight = false;
+local _isEyeforanEye = false;
+local _isConsecration = false;
+local _isCrusade = false;
+local _isDivineHammer = false;
 
-----------------------------------------------
--- Pre enable, checking talents
-----------------------------------------------
-TDDps_Paladin_CheckTalents = function()
-	_isFinalVerdict = TD_TalentEnabled('Final Verdict');
-	_isSeraphim = TD_TalentEnabled('Seraphim');
-	_isSanctifiedWrath = TD_TalentEnabled('Sanctified Wrath');
-	_isExecutionSentence = TD_TalentEnabled('Execution Sentence');
-	_isLightsHammer = TD_TalentEnabled('Light\'s Hammer');
-	_isHolyPrism = TD_TalentEnabled('Holy Prism');
-	_isSacredShield = TD_TalentEnabled('Sacred Shield');
+MaxDps.Paladin = {};
+
+function MaxDps.Paladin.CheckTalents()
+	MaxDps:CheckTalents();
+	_isFinalVerdict = MaxDps:HasTalent(_FinalVerdict);
+	_isExecutionSentence = MaxDps:HasTalent(_ExecutionSentence);
+	_isZeal = MaxDps:HasTalent(_Zeal);
+	_isCrusade = MaxDps:HasTalent(_Crusade);
+	_isBlindingLight = MaxDps:HasTalent(_BlindingLight);
+	_isEyeforanEye = MaxDps:HasTalent(_EyeforanEye);
+	_isConsecration = MaxDps:HasTalent(_Consecration);
+	_isDivineHammer = MaxDps:HasTalent(_DivineHammer);
 	-- other checking functions
 end
 
-----------------------------------------------
--- Enabling Addon
-----------------------------------------------
-function TDDps_Paladin_EnableAddon(mode)
+function MaxDps:EnableRotationModule(mode)
 	mode = mode or 1;
-	_TD["DPS_Description"] = "TD Paladin DPS supports: Retribution";
-	_TD["DPS_OnEnable"] = TDDps_Paladin_CheckTalents;
+	MaxDps.Description = 'Paladin Module [Retribution]';
+	MaxDps.ModuleOnEnable = MaxDps.Paladin.CheckTalents;
 	if mode == 1 then
-		_TD["DPS_NextSpell"] = TDDps_Paladin_Holy;
+		MaxDps.NextSpell = MaxDps.Paladin.Holy;
 	end;
 	if mode == 2 then
-		_TD["DPS_NextSpell"] = TDDps_Paladin_Protection;
+		MaxDps.NextSpell = MaxDps.Paladin.Protection;
 	end;
 	if mode == 3 then
-		_TD["DPS_NextSpell"] = TDDps_Paladin_Retribution;
+		MaxDps.NextSpell = MaxDps.Paladin.Retribution;
 	end;
-	TDDps_EnableAddon();
 end
 
-----------------------------------------------
--- Main rotation: Holy
-----------------------------------------------
-TDDps_Paladin_Holy = function()
-	local timeShift, currentSpell = TD_EndCast();
+function MaxDps.Paladin.Holy()
+	local timeShift, currentSpell = MaxDps:EndCast();
 
-	return _Spell;
+	return nil;
 end
 
-----------------------------------------------
--- Main rotation: Protection
-----------------------------------------------
-TDDps_Paladin_Protection = function()
-	local timeShift, currentSpell = TD_EndCast();
+function MaxDps.Paladin.Protection()
+	local timeShift, currentSpell = MaxDps:EndCast();
+
+	return nil;
+end
+
+function MaxDps.Paladin.Retribution()
+	local timeShift, currentSpell, gcd = MaxDps:EndCast();
+
+	local crusStrike = _CrusaderStrike;
+	if _isZeal then
+		crusStrike = _Zeal;
+	end
+
+	local bladeOfJustice = _BladeofJustice;
+	if _isDivineHammer then
+		bladeOfJustice = _DivineHammer;
+	end
 
 	local holyPower = UnitPower('player', SPELL_POWER_HOLY_POWER);
-	local cs, csCD = TD_SpellAvailable(_CrusaderStrike, timeShift);
-	local j, jCD = TD_SpellAvailable(_Judgment, timeShift);
-	local hw = TD_SpellAvailable(_HolyWrath, timeShift);
-	local as = TD_SpellAvailable(_AvengersShield, timeShift);
-	local es = TD_SpellAvailable(_ExecutionSentence, timeShift);
-	local lh = TD_SpellAvailable(_LightsHammer, timeShift);
-	local hp = TD_SpellAvailable(_HolyPrism, timeShift);
-	local sera, seraCd = TD_SpellAvailable(_Seraphim, timeShift);
-	local how = TD_SpellAvailable(_HammerofWrath, timeShift);
-	local con = TD_SpellAvailable(_Consecration, timeShift);
-	local ssCD = TD_SpellAvailable(_SacredShield, timeShift);
-	local sotr = TD_SpellAvailable(_ShieldoftheRighteous); --not on gcd
+	local jAura, jAuraCD = MaxDps:TargetAura(_Judgment, timeShift + 0.5);
+	local dp = MaxDps:Aura(_DivinePurpose, timeShift);
+	local j, jCD = MaxDps:SpellAvailable(_Judgment, timeShift);
+	local _, crusadeCD = MaxDps:SpellAvailable(_Crusade, timeShift);
+	local crus, crusCD = MaxDps:SpellAvailable(crusStrike, timeShift);
+	local boj, bojCD = MaxDps:SpellAvailable(bladeOfJustice, timeShift);
 
-	local avAura = TD_Aura(_AvengingWrath, timeShift);
-	local ss = TD_Aura(_SacredShield, timeShift + 7);
+	local targetPh = MaxDps:TargetPercentHealth();
 
-	local targetPh = TD_TargetPercentHealth();
+	MaxDps:GlowCooldown(_AvengingWrath, MaxDps:SpellAvailable(_AvengingWrath, timeShift));
+	MaxDps:GlowCooldown(_ShieldofVengeance, MaxDps:SpellAvailable(_ShieldofVengeance, timeShift));
+	MaxDps:GlowCooldown(_BlindingLight, _isBlindingLight and MaxDps:SpellAvailable(_BlindingLight, timeShift));
+	MaxDps:GlowCooldown(_EyeforanEye, _isEyeforanEye and MaxDps:SpellAvailable(_EyeforanEye, timeShift));
+	MaxDps:GlowCooldown(_Crusade, _isCrusade and MaxDps:SpellAvailable(_Crusade, timeShift));
 
-	TDButton_GlowCooldown(_ShieldoftheRighteous, holyPower >= 3 and sotr);
-	TDButton_GlowCooldown(_SacredShield, not ss and ssCD);
-
-	if cs then
-		return _CrusaderStrike;
-	end
-
-	if j then
-		return _Judgment;
-	end
-
-	if _isSanctifiedWrath and hw then
-		return _HolyWrath;
-	end
-
-	if as then
-		return _AvengersShield;
-	end
-
-	if not _isSeraphim or seraCd < 2 then
-		if _isExecutionSentence and es then
-			return _ExecutionSentence;
-		end
-
-		if _isLightsHammer and lh then
-			return _LightsHammer;
-		end
-
-		if _isHolyPrism and hp then
-			return _HolyPrism;
-		end
-	end
-
-	if (targetPh <= 0.2 or avAura) and how then
-		return _HammerofWrath;
-	end
-
-	if con then
-		return _Consecration;
-	end
-
-	if not _isSanctifiedWrath and hw then
-		return _HolyWrath;
-	end
-
-	if csCD < jCD then
-		return _CrusaderStrike;
-	else
-		return _Judgment;
-	end
-end
-
-----------------------------------------------
--- Main rotation: Retribution
-----------------------------------------------
-TDDps_Paladin_Retribution = function()
-	local timeShift, currentSpell = TD_EndCast();
-	local gcd = TD_GlobalCooldown();
-
-	local holyPower = UnitPower('player', SPELL_POWER_HOLY_POWER);
-	local finalVerdict = TD_Aura(_FinalVerdict, timeShift);
-	local avAura = TD_Aura(_AvengingWrath, timeShift);
-	local eds = TD_Aura(_EmpoweredDivineStorm, timeShift);
-	local ds = TD_Aura(_DivineCrusader, timeShift);
-	local es = TD_SpellAvailable(_ExecutionSentence, timeShift);
-	local how = TD_SpellAvailable(_HammerofWrath, timeShift);
-	local av = TD_SpellAvailable(_AvengingWrath, timeShift);
-	local cs, csCD = TD_SpellAvailable(_CrusaderStrike, timeShift);
-	local j, jCD = TD_SpellAvailable(_Judgment, timeShift);
-	local e, eCD = TD_SpellAvailable(_Exorcism, timeShift);
-	local targetPh = TD_TargetPercentHealth();
-	local sera, seraCd = TD_SpellAvailable(_Seraphim, timeShift);
-	TDButton_GlowCooldown(_AvengingWrath, av);
-
-	if _isSeraphim then
-		if seraCd < gcd and holyPower >= 5 then
-			return _Seraphim;
-		end
-	end
-
-	if finalVerdict and holyPower >= 4 and (eds or ds) then
-		return _DivineStorm;
-	end
-
-	if holyPower >= 5 then
-		if _isSeraphim then
-			if seraCd > 4 * gcd then
-				return _TemplarVerdict;
-			end
-		elseif _isFinalVerdict then
-			return _FinalVerdict;
-		else
-			return _TemplarVerdict;
-		end
-	end
-
-	if es then
+	if _isExecutionSentence and MaxDps:SpellAvailable(_ExecutionSentence, timeShift) and holyPower >= 3 and (
+		(jCD < gcd * 4.5) or (jAuraCD > gcd * 4.67)) and
+		(not _isCrusade or (crusadeCD > gcd * 2)
+	)
+	then
 		return _ExecutionSentence;
 	end
 
-	if (targetPh <= 0.35 or avAura) and how then
-		return _HammerofWrath;
+	if jAura and (holyPower > 2 or dp) then
+		return _TemplarsVerdict;
 	end
 
-	if cs then
-		return _CrusaderStrike;
+	if MaxDps:SpellAvailable(_WakeofAshes, timeShift) and holyPower <= 1 then
+		return _WakeofAshes;
 	end
 
-	if j then
+	if crus and holyPower < 5 then
+		return crusStrike;
+	end
+
+	if boj and holyPower <= 3 then
+		return bladeOfJustice;
+	end
+
+	if j and holyPower >= 3 then
 		return _Judgment;
 	end
 
-	if e then
-		return _Exorcism;
+	if _isConsecration and MaxDps:SpellAvailable(_Consecration, timeShift) then
+		return _Consecration;
 	end
 
-	if holyPower >= 3 then
-		if _isSeraphim then
-			if seraCd > 8 * gcd then
-				return _TemplarVerdict;
-			end
-		elseif _isFinalVerdict then
-			return _FinalVerdict;
-		else
-			return _TemplarVerdict;
-		end
-	end
-
-	if eds or ds then
-		return _DivineStorm;
-	end
-
-	if csCD < jCD and csCD < eCD then
-		return _CrusaderStrike;
-	end
-
-	if jCD < csCD and jCD < eCD then
+	if holyPower >= 3 and jCD < bojCD and jCD < crusCD then
 		return _Judgment;
 	end
 
-	if eCD < csCD and eCD < jCD then
-		return _Exorcism;
+	if bojCD < crusCD then
+		return bladeOfJustice;
+	else
+		return crusStrike;
 	end
-
-	return _CrusaderStrike;
 end
